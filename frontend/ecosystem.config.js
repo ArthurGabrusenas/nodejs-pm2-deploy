dc264112262116c0f757ba00764eadc34099ec80
a.gabrusenas <gabrusenas@outlook.com>
2026-01-12 03:52:55 +0300

    Добавлен KEY

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,6 +5,7 @@ const REPO = process.env.REPO;
 const BRANCH = process.env.BRANCH;
 const FRONTEND_PATH = process.env.DEPLOY_FRONTEND_PATH;
 const HOST = process.env.HOST;
+const KEY = process.env.KEY;
 
 module.exports = {
   deploy: {
@@ -13,6 +14,7 @@ module.exports = {
       repo: REPO,
       host: HOST,
       path: FRONTEND_PATH,
+      key: KEY,
       ssh_options: 'StrictHostKeyChecking=no',
       'post-deploy': `cd ${FRONTEND_PATH}/current/frontend && npm ci && npm run build`
     },
