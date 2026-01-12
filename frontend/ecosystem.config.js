6a171757a35e33168cb48836128cc64d396f77fc
a.gabrusenas <gabrusenas@outlook.com>
2026-01-12 03:50:29 +0300

    Добавлен ecosystem.config для frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env.deploy') });
+
+const REPO = process.env.REPO;
+const BRANCH = process.env.BRANCH;
+const FRONTEND_PATH = process.env.DEPLOY_FRONTEND_PATH;
+const HOST = process.env.HOST;
+
+module.exports = {
+  deploy: {
+    production: {
+      ref: BRANCH,
+      repo: REPO,
+      host: HOST,
+      path: FRONTEND_PATH,
+      ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy': `cd ${FRONTEND_PATH}/current/frontend && npm ci && npm run build`
+    },
+  },
+};
